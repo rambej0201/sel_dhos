f8e3ffcf1364a630e505baeb5a7c53e982b12a6f
rambej0201 <1503010201@svuca.edu>
2021-03-14 19:56:43 -0700

    Adding testNG part on the implementaion

--- selFramWork/src/test/java/com/rss/tets/DemoTestNG.java
+++ selFramWork/src/test/java/com/rss/tets/DemoTestNG.java
@@ -1,4 +1,4 @@
-package com.rss.sample;
+package com.rss.tets;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
@@ -6,13 +6,22 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
 
-public class WebLaunchTest {
-    public static WebDriverManager webDriverManager;
+public class DemoTestNG {
+    public WebDriverManager webDriverManager;
+    public WebDriver driving_chrome = null;
 
-    static void searchingSomethingOnGoogle() throws InterruptedException {
+    @BeforeTest
+    public void setUpTest(){
         webDriverManager.chromedriver().setup();
-        WebDriver driving_chrome = new ChromeDriver();
+        driving_chrome = new ChromeDriver();
+    }
+
+    @Test
+    public void searchingSomethingOnGoogle() throws InterruptedException {
         driving_chrome.get("https://www.google.com");
         System.out.println("Lanched Google website on chrome");
         Thread.sleep(3000);
@@ -32,12 +41,13 @@ public class WebLaunchTest {
         driving_chrome.findElement(By.name("btnK")).sendKeys(Keys.RETURN);
         Thread.sleep(2000);
         System.out.println("Waiting for some seconds4");
-        driving_chrome.close();
-        driving_chrome.quit(); 
-        System.out.println("closed and quite driving_chrome");
-    }
-    public static void main(String[] args) throws InterruptedException {
-        searchingSomethingOnGoogle();
+        System.out.println("In test method");
     }
 
+    @AfterTest
+    public void tearDownTest(){
+        driving_chrome.close();
+        driving_chrome.quit();
+        System.out.println("closed and quite driving_chrome");
+    }
 }
